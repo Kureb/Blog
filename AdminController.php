09f9fd0f380929d6e7ca6b95c392d6787e7ea1a0
Alexandre DAUSSY <alexandre.daussy@gmail.com>
2013-12-13 19:12:11 +0100

    Classe AdminController qui permet la gestion du blog

---
+++ AdminController.php
@@ -0,0 +1,67 @@
+<?php
+/*
+ * Gestion du blog
+ * BOULANGER Vincent & DAUSSY Alexandre
+ */
+class AdminController{
+
+
+	/* Affichage d'un formulaire pour ajouter un message */
+	public function addMessage($param){
+		echo "Methode : " . __FUNCTION__ . "<br> Parametre : " . $param . "<br>";
+	}
+
+	/* Envoi des données pour ajouter un message */
+	public function saveMessage($param){
+		echo "Methode : " . __FUNCTION__ . "<br> Parametre : " . $param . "<br>";
+
+	}
+
+	/* Affichage d'un formulaire pour ajouter une catégorie */
+	public function addCategorie($param){
+		echo "Methode : " . __FUNCTION__ . "<br> Parametre : " . $param . "<br>";
+	}
+
+	/* Envoi des données pour ajouter une catégorie */
+	public function saveCategorie($param){
+		echo "Methode : " . __FUNCTION__ . "<br> Parametre : " . $param . "<br>";
+	}
+
+
+	public function callAction($tab){
+		//Voir BlogController.php pour voir les commentaires
+		foreach ($tab as $key => $value) {
+			switch ($value) {
+				case 'addM':
+					$this->addMessage($key);
+					break;
+
+				case 'saveM':
+					$this->saveMessage($key);
+					break;
+
+				case 'addC':
+					$this->addCategorie($key);
+					break;
+
+				case 'saveC':
+					$this->saveCategorie($key);
+					break;
+
+				default:
+					echo "action impossible <br>";
+					break;
+			}
+		}
+	}
+
+	
+
+}
+/*
+$a = new AdminController();
+$a->addMessage(5);
+$lol = "lol";
+$a->addCategorie($lol);
+*/
+?>
