f24947d2fa3560f262700a51f9c44e33ed7f1cd9
Alexandre DAUSSY <alexandre.daussy@gmail.com>
2013-12-19 17:04:32 +0100

    Affichage amélioré, ajout d'un getter dans catégorie

--- Affichage.php
+++ Affichage.php
@@ -5,11 +5,15 @@ include_once 'Categorie.php';
 
 class Affichage{
 
-	function affichageGeneral($articles/*$contenu_central, $menu_droite, $menu_gauche*/){
+	function affichageGeneral($articles, $categorie/*$contenu_central, $menu_droite, $menu_gauche*/){
 		$file = 'Blog1.html';
 		$content = file_get_contents($file);
-		$search = "mesarticles";
-		$content = str_replace($search, "$articles", $content);
+		$artc = "mesarticles";
+		$cat = "mescategories";
+		$link = "(suite)";
+		$content = str_replace($artc, "$articles", $content);
+		$content = str_replace($cat, "$categorie", $content);
+		$content = str_replace($link, '<a href="http://9gag.com/">(suite)</a>', $content);
 		echo $content;
 }
 
@@ -17,10 +21,10 @@ class Affichage{
 
 	/* Fonction qui retourne le code HTML pour un billet */
 	static function afficherBillet($billet){
-		$code = "<div id = \"Article\" >" .
-				"<h1>" . $billet->getAttr("titre") . "</h1><br>" . 
-				"<p>" . $billet->getAttr("body") . "</p>" . 
-				"<p>" . $billet->getAttr("date") . "</p>" .
+		$code = "<div id = \"Article\" >\n" .
+				"<h1>" . $billet->getAttr("titre") . "</h1><br>\n" . 
+				"<p>" . $billet->getAttr("body") . "</p>\n" . 
+				"<p>" . $billet->getAttr("date") . "</p>\n" .
 				"</div>";
 
 		return $code;
@@ -36,16 +40,32 @@ class Affichage{
 			$code = $code . "<table>\n";
 			foreach($liste as $billet){
 				$code = $code . "<tr><td>\n";
-				$code = $code . "<div id = \"Article\" >" .
-						"<h1>" . $billet->getAttr("titre") . "</h1><br>" .
-						"<p>" . substr($billet->getAttr("body"),0,20) . "...(suite)</p>" .
-						"<p>" . $billet->getAttr("date") . "</p>" .
-						"</div>";
-				$code = $code . "</div>";
+				$code = $code . "<div id = \"Article\" >\n" .
+						"<h1>" . $billet->getAttr("titre") . "</h1><br>\n" .
+						"<p>" . substr($billet->getAttr("body"),0,220) . "...(suite)</p>\n" .
+						"<i>" . "publié le " . $billet->getAttr("date") . "</i>\n" .
+						"</div>\n";
+				$code = $code . "</div>\n";
 				//$code = $code . self::afficherBillet($billet);
 				$code = $code . "\n</td></tr>\n";
 			}
-			$code = $code . "</table>";
+			$code = $code . "</table>\n";
+		}
+		return $code;
+	}
+
+
+	/* Retourne le code HTML pour afficher tous les catégories
+	 * présentent dans la BDD*/
+	static function afficheListeCategorie($liste){
+		$code = "";
+		if(sizeof($liste)==0)
+			$code = "Aucune catégorie";
+		else{
+			$code = "<b>Liste des catégories</b><br>\n\t";
+			foreach ($liste as $categorie) {
+				$code = $code . $categorie->getAttr("titre") . " <br>\n\t";
+			}
 		}
 		return $code;
 	}
@@ -61,7 +81,9 @@ $a->affichageGeneral($c);
 */
 
 $b = Billet::findAll();
+$cat = Categorie::findAll();
+$lol = $a->afficheListeCategorie($cat);
 $c = $a->afficheListeBillets($b);
-$a->affichageGeneral($c);
+$a->affichageGeneral($c, $lol);
 
 ?>
--- Categorie.php
+++ Categorie.php
@@ -90,6 +90,14 @@ class Categorie {
     throw new Exception($emess, 45);
   }
 
+  public function getAttr($attr_name) {
+    if (property_exists( __CLASS__, $attr_name)) { 
+      return $this->$attr_name;
+    } 
+    $emess = __CLASS__ . ": unknown member $attr_name (getAttr)";
+    throw new Exception($emess, 45);
+  }
+
   
   /**
    *   Setter magique
