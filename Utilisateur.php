06f5881972af796d1fbc21c7574740dd51056a45
Alexandre DAUSSY <alexandre.daussy@gmail.com>
2013-12-23 23:09:16 +0100

    Correction méthode findByLogin

--- Utilisateur.php
+++ Utilisateur.php
@@ -91,28 +91,6 @@ class Utilisateur{
 	}
 
 
-
-	public static function findById($userid){
-		$c = Base::getConnection();
-		$query = 'select * from utilisateurs where userid= :userid';
-		$dbres = $c->prepare($query);
-		$dbres->bindParam(':userid', $userid);
-		$dbres->execute();
-		$user = false;
-		$d = $dbres->fetch(PDO::FETCH_OBJ);
-
-		$user = new Utilisateur();
-		$user->setAttr("userid", $d->userid);
-		$user->setAttr("login", $d->login);
-		$user->setAttr("password", $d->password);
-		$user->setAttr("mail", $d->mail);
-
-		return $user;
-	}
-
-
-
-
 	public static function findAll(){
 		$query = "select * from utilisateurs";
 		$c = Base::getConnection();
@@ -133,19 +111,75 @@ class Utilisateur{
 	}
 
 
+	public static function findById($userid){
+		$c = Base::getConnection();
+		$query = 'select * from utilisateurs where userid= :userid';
+		$dbres = $c->prepare($query);
+		$dbres->bindParam(':userid', $userid);
+		$dbres->execute();
+		//$user = false;
+		$d = $dbres->fetch(PDO::FETCH_OBJ);
+		if($d == false) return false;
+		$user = new Utilisateur();
+		$user->setAttr("userid", $userid);
+		$user->setAttr("login", $d->login);
+		$user->setAttr("password", $d->password);
+		$user->setAttr("mail", $d->mail);
+
+		return $user;
+	}
 
 
-	public static function findByLogin($login){
+	public static function findByLogin($login) {
 		$c = Base::getConnection();
 		$query = 'select * from utilisateurs where login= :login';
 		$dbres = $c->prepare($query);
 		$dbres->bindParam(':login', $login);
-		$user = false;
+		$dbres->execute();
+		//$user = false;
 		$d = $dbres->fetch(PDO::FETCH_OBJ);
+		if($d == false) return false;
+		$user = new Utilisateur();
+		$user->setAttr("login", $login);
+		$user->setAttr("userid", $d->userid);
+		$user->setAttr("password", $d->password);
+		$user->setAttr("mail", $d->mail);
 
+		return $user;
+
+	}
+
+	public static function findByLogin2($login) {
+		$c = Base::getConnection();
+		$query = 'select * from utilisateurs where login = :login';
+		$dbres = $c->prepare($query);
+		$dbres->bindParam(':login', $login);
+		$dbres->execute();
+		$d = $dbres->fetch(PDO::FETCH_OBJ);
+		if($d == false) return false;
+		$util = new Utilisateur();
+		$d=$dbres->fetch(PDO::FETCH_OBJ);
+	    $util->setAttr("userid", $d->getAttr("userid"));
+	    $util->setAttr("login", $d->getAttr("login"));
+	    $util->setAttr("password",$d->getAttr("password"));
+	    $util->setAttr("mail", $d->getAttr("mail"));
+		
+	  	return $util;
+	}
+
+
+	public static function findByLoginmoi($login){
+		$c = Base::getConnection();
+		$query = 'select * from utilisateurs where login= :login';
+		$dbres = $c->prepare($query);
+		$dbres->bindParam(':login', $login);
+		$dbres->execute();
+		//$user = false;
+		$d = $dbres->fetch(PDO::FETCH_OBJ);
+		if($d==false) return false;
 		$user = new Utilisateur();
 		$user->setAttr("userid", $d->userid);
-		$user->setAttr("login", $d->login);
+		$user->setAttr("login", $login);
 		$user->setAttr("password", $d->password);
 		$user->setAttr("mail", $d->mail);
 
