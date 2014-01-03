063f4b922ad837e34917acb675013d62f85dc44c
Alexandre DAUSSY <alexandre.daussy@gmail.com>
2014-01-03 22:14:52 +0100

    Déconnexion et ébauche espace membre

---
+++ membre.php
@@ -0,0 +1,19 @@
+<?php 
+
+session_start();
+if (!isset($_SESSION['login'])){
+	header('Location: connexion.php');
+}
+
+?>
+
+<html>
+	<head>
+	<title>Mon espace membre</title>
+	</head>
+	<body>
+		Bienvenue <?php echo $_SESSION['login']; ?><br>
+		<a href="deconnexion.php">D&eacute;connexion</a>
+
+	</body>
+
