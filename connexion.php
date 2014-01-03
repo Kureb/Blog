eea9bbf21e48e06fbfdda47cbbfb9b9b6a3d3c37
Alexandre DAUSSY <alexandre.daussy@gmail.com>
2014-01-03 21:49:48 +0100

    Connexion fonctionnelle

--- connexion.php
+++ connexion.php
@@ -1,10 +1,44 @@
 <?php
 include_once "Utilisateur.php";
 
-echo '
+
+$log = '';
+
+if (isset($_POST['connexion']) && $_POST['connexion'] == 'Connexion'){
+	if (isset($_POST['pseudo']) && !empty($_POST['pseudo'])
+		&& (isset($_POST['pass']) && !empty($_POST['pass']))){
+
+		$pseudo = htmlentities($_POST['pseudo']);
+		$user = Utilisateur::findByLogin($pseudo);
+
+		if($user==false){
+			$log .= "Cet utilisateur n'existe pas.<br>";
+		}else{
+			$mdp = $user->getAttr("password");
+			if($mdp != sha1($_POST['pass'])){
+				$log .= 'Mot de passe incorrecte.<br>';
+			}else{
+				$log .= 'Connexion en cours.<br>';
+				session_start();
+				$_SESSION['pseudo'] = $_POST['pseudo'];
+				header("Refresh: 3; url=Blog.php"); 
+				//header('Location: Blog.php');
+			}
+		}
+
+
+	}else{
+		$log .= 'Au moins un champ est vide.<br>';
+	}
+}
+
+?>
+
+
+
 <html>
 	<head>
-		<title>Connexion</title>
+		<title>Connection</title>
 		<meta charset="utf-8" />
 	</head>
 
@@ -12,49 +46,13 @@ echo '
 		<div id="formulaire">
 		<form action="connexion.php" method="post">
 	 		Pseudo : 
-	 			<input type="text" name="pseudo"/><br>
+	 			<input type="text" name="pseudo" value="<?php if (isset($_POST['pseudo'])) echo htmlentities(trim($_POST['pseudo'])); ?>"/><br>
 			Mot de passe : 
-				<input type="password" name="mdp"/><br>
-						
-			<input type="submit" value="Valider" />
+				<input type="password" name="pass" value="<?php if (isset($_POST['pass'])) echo htmlentities(trim($_POST['pass']))?>"/><br>
+			
+			<input type="submit" name="connexion" value="Connexion" />
 		</form>
-
+		<?php if (isset($log)) echo '<br />' . $log; ?>
 		</div>
 	</body>
-
-
-</html>';
-
-/* Pour éviter l'usage des balises dans les champs
- * j'utilise htmlentities */
-if((sizeof($_POST['pseudo'])==0) && (sizeof($_POST['mdp'])==0)){
-	$pseudo=htmlentities($_POST['pseudo']);
-	$user = Utilisateur::findByLogin($pseudo);
-	if($user!=false) {
-		$mdp=htmlentities($_POST['mdp']);
-		if(sha1($mdp)==$user->getAttr("password"))
-			echo "connexion possible";
-		else
-			echo "mdp incorrecte";
-	}else{
-		echo "utilisateur inconnu";
-	}
-}else{
-	echo "remplissez les 2 champs svp";
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-?>
+</html>
