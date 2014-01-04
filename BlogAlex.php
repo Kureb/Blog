447fb4f2db97774c9c0ecd277544c4040682e30d
Alexandre DAUSSY <alexandre.daussy@gmail.com>
2014-01-04 18:25:28 +0100

    Merge des modif de Vincent et des miens (sauf footer)

--- BlogAlex.php
+++ BlogAlex.php
@@ -1,45 +1,37 @@
 
 <html>
-			<head>
-				<title>EMIN3M rules them'al</title>
-		        <meta charset="utf-8" />
-				<style type="text/css"></style>
-				<link rel="stylesheet" href="BlogAlex.css" />
-			</head>
+	<head>
+		<title>Projet Web</title>
+		<meta charset="utf-8" />
+		<style type="text/css"></style>
+		<link rel="stylesheet" href="BlogAlex.css" />
+	</head>
 
-			<body>
-			<div id="page">
-			<div id="principal">
-			<div id="banniere">
-				<img id="ban" src="LOL.jpg">
-			</div>
-
-		<!-- 	<div id="share">
-				<img id="sharing" src="Share.png">
-			</div>
-		 -->
-			<div id="gauche">
-				<img id="sharing" src="Share.png">
-				<p id="Auteur">
-					auteurlol
-				</p>
-				<p id="categorie">
-					mescategories
-				</p>
-			</div>
-
-			<div id="droite">
-				mesarticles
-			</div>
-		
-			<!-- " . $b . " -->
-			<!-- <div id="footer">
-+		<p>
-+			Ici devra siègera fièrement le roi Footer
-+		</p>
-+	</div> -->
-			</div>
+	<body>
+		<div id="page">
+		<div id="principal">
+		<div id="banniere">
+			<img id="ban" src="LOL.jpg">
 		</div>
-			<script href="scriptder.js"></script>
-			</body>
-		</html>
+
+		<div id="gauche">
+			<img id="sharing" src="Share.png">
+			<p id="Auteur">
+				auteurlol
+			</p>
+			<p id="categorie">
+				mescategories
+			</p>
+		</div>
+
+		<div id="droite">
+			mesarticles
+		</div>
+
+		</div> <!-- principal -->
+
+		</div> <!-- page -->
+		
+		<script href="scriptder.js"></script>
+	</body>
+</html>
