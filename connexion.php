c7bf6fa91f2f12058dc11c30d1c0b633e8de8dbe
Alexandre DAUSSY <alexandre.daussy@gmail.com>
2014-01-05 14:30:27 +0100

    Mise en page de la  connexion

--- connexion.php
+++ connexion.php
@@ -2,10 +2,10 @@
 include_once "Utilisateur.php";
 
 
-$log = '';
+//$log = '';
 session_start();
 if (!empty($_SESSION['login'])){
-	$log .= 'Vous êtes déjà connecté, vous ne pouvez pas vous reconnecter.<br>';
+	$log = 'Vous êtes déjà connecté, vous ne pouvez pas vous reconnecter.<br>';
 	header("Refresh: 2, url=membre.php");
 }
 
@@ -17,13 +17,13 @@ if (isset($_POST['connexion']) && $_POST['connexion'] == 'Connexion'){
 		$user = Utilisateur::findByLogin($pseudo);
 
 		if($user==false){
-			$log .= "Cet utilisateur n'existe pas.<br>";
+			$log = "Cet utilisateur n'existe pas.<br>";
 		}else{
 			$mdp = $user->getAttr("password");
 			if($mdp != sha1($_POST['pass'])){
-				$log .= 'Mot de passe incorrecte.<br>';
+				$log = 'Mot de passe incorrecte.<br>';
 			}else{
-				$log .= 'Connexion en cours.<br>';
+				$log = 'Connexion en cours.<br>';
 				//session_start();
 				$_SESSION['login'] = $_POST['pseudo'];
 				header("Refresh: 1; url=membre.php"); 
@@ -33,7 +33,7 @@ if (isset($_POST['connexion']) && $_POST['connexion'] == 'Connexion'){
 
 
 	}else{
-		$log .= 'Au moins un champ est vide.<br>';
+		$log = 'Au moins un champ est vide.<br>';
 	}
 }
 
@@ -45,19 +45,25 @@ if (isset($_POST['connexion']) && $_POST['connexion'] == 'Connexion'){
 	<head>
 		<title>Connexion</title>
 		<meta charset="utf-8" />
+		<style type="text/css"></style>
+		<link rel="stylesheet" href="test.css" />
 	</head>
 
 	<body>
 		<div id="formulaire">
 		<form action="connexion.php" method="post">
-	 		Pseudo : 
-	 			<input type="text" name="pseudo" value="<?php if (isset($_POST['pseudo'])) echo htmlentities(trim($_POST['pseudo'])); ?>"/><br>
-			Mot de passe : 
-				<input type="password" name="pass" value="<?php if (isset($_POST['pass'])) echo htmlentities(trim($_POST['pass']))?>"/><br>
-			
+		<div class="center">
+			<span class="label">Pseudo : </span>
+	 		<input class="champ" type="text" name="pseudo" value="<?php if (isset($_POST['pseudo'])) echo htmlentities(trim($_POST['pseudo'])); ?>"/><br>
+			<span class="label">Mot de passe : </span>
+			<input class="champ" type="password" name="pass" value="<?php if (isset($_POST['pass'])) echo htmlentities(trim($_POST['pass']))?>"/><br>
 			<input type="submit" name="connexion" value="Connexion" />
+		</div>
 		</form>
-		<?php if (isset($log)) echo '<br />' . $log; ?>
+		<?php 
+		if (isset($log))
+			echo '<div class="message">'.$log.'</div>'; 
+		 ?>
 		</div>
 	</body>
 </html>
